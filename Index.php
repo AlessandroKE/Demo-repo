d042ef3c01b994ac13ab0cf64fdc015b12c48d7e
SandroKE <koomealessandro@gmail.com>
2023-02-20 17:13:36 +0300

    Added index.php

---
+++ Index.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = "Alessandro";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>I love php and laravel</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+    <h1>Kenya To the World</h1>
+
+    <p>Hello, <?= $name; ?>!</p>
+
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Demo-repo
 Studying Git and Github
 Refresher on Git and Github
+##I love coding in php and Python
